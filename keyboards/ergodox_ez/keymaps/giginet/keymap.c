94cf22d89f76ab1e53bbbd103c2a36c1d4e522c8
giginet <giginet.net@gmail.com>
2018-07-11 23:40:41 +0900

    Fix settings for main layer

--- keyboards/ergodox_ez/keymaps/giginet/keymap.c
+++ keyboards/ergodox_ez/keymaps/giginet/keymap.c
@@ -9,6 +9,9 @@
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
 
+#define KC_KANA  KC_LANG1
+#define KC_ALPHA KC_LANG2
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
@@ -35,23 +38,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
+        KC_ESC,        KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
         KC_GRV,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_NO,
         KC_TAB,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LCTL,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   KC_NO,
-        KC_F1,KC_QUOT,    KC_LCTL,  KC_LALT,KC_LGUI,
+        KC_LCTL,       CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   KC_KANA,
+        KC_F1,KC_QUOT, KC_LCTL,  KC_LALT,KC_LGUI,
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_LANG2,
-                                               KC_SPC,KC_LGUI,KC_LANG1,
+                                              KC_SPC,KC_LGUI, MO(SYMB),
         // right hand
-             KC_NO,KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_BSPC,
-             TG(SYMB),       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
-             KC_TRNS,KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_MINS,  KC_PLUS,KC_LBRC,KC_RBRC,          MO(SYMB),
-             KC_LEFT,        KC_RIGHT,
-             KC_UP,
-             KC_DOWN,KC_RGUI, KC_SPACE
+        KC_NO,         KC_6,         KC_7,   KC_8,   KC_9,   KC_0,             KC_BSPC,
+        KC_NO,         KC_Y,         KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
+                       KC_H,         KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
+        KC_ALPHA,      KC_N,         KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+                                     KC_MINS,KC_PLUS,KC_LBRC,KC_RBRC,          MO(SYMB),
+        KC_LEFT,        KC_RIGHT,
+        KC_UP,
+        MO(MDIA),KC_TAB, KC_RGUI
     ),
 /* Keymap 1: Symbol Layer
  *
