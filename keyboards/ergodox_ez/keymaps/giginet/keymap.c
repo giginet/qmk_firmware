afe61cee59fda588fab0d854b1be1366367ac568
giginet <giginet.net@gmail.com>
2018-07-11 23:58:47 +0900

    Update audio layer

--- keyboards/ergodox_ez/keymaps/giginet/keymap.c
+++ keyboards/ergodox_ez/keymaps/giginet/keymap.c
@@ -9,9 +9,6 @@
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
 
-#define KC_KANA  KC_LANG1
-#define KC_ALPHA KC_LANG2
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
@@ -41,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,        KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
         KC_GRV,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_NO,
         KC_TAB,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LCTL,       CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   KC_KANA,
+        KC_LCTL,       CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   KC_LANG1,
         KC_F1,KC_QUOT, KC_LCTL,  KC_LALT,KC_LGUI,
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_LANG2,
@@ -50,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,         KC_6,         KC_7,   KC_8,   KC_9,   KC_0,             KC_BSPC,
         KC_NO,         KC_Y,         KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                        KC_H,         KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
-        KC_ALPHA,      KC_N,         KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+        KC_LANG2,      KC_N,         KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                                      KC_MINS,KC_PLUS,KC_LBRC,KC_RBRC,          MO(SYMB),
         KC_LEFT,        KC_RIGHT,
         KC_UP,
@@ -121,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // MEDIA AND MOUSE
 [MDIA] = LAYOUT_ergodox(
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_SLCK, KC_PAUS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -130,8 +127,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                     KC_TRNS,
                                   KC_TRNS, KC_TRNS, KC_TRNS,
     // right hand
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, KC_EJCT,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_TRNS,
                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
                           KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
