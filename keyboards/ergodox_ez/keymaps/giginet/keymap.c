a5ebea8c36021f003ac9fb61f7ae6b7407fed4ec
giginet <giginet.net@gmail.com>
2018-07-24 16:22:19 +0900

    Add symbols

--- keyboards/ergodox_ez/keymaps/giginet/keymap.c
+++ keyboards/ergodox_ez/keymaps/giginet/keymap.c
@@ -13,9 +13,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | ESC    |   1  |   2  |   3  |   4  |   5  |      |            |      |   6  |   7  |   8  |   9  |   0  |   BS   |
+ * | ESC    |   1  |   2  |   3  |   4  |   5  |  -_  |           |  =+  |   6  |   7  |   8  |   9  |   0  |   BS   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | `      |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | `      |   Q  |   W  |   E  |   R  |   T  |  [   |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |LCtl/Tab|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   '"   |
  * |--------+------+------+------+------+------| Kana |           | Alpha|------+------+------+------+------+--------|
@@ -35,8 +35,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,        KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_NO,
-        KC_GRV,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_NO,
+        KC_ESC,        KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_MINS,
+        KC_GRV,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,
         CTL_T(KC_TAB), KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,       CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   KC_LANG1,
         MO(MDIA),      KC_QUOT,      KC_LCTL,KC_LALT,KC_LGUI,
@@ -44,8 +44,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                              KC_UP,
                                              KC_SPC,KC_LGUI, KC_DOWN,
         // right hand
-        KC_NO,         KC_6,         KC_7,   KC_8,   KC_9,   KC_0,             KC_BSPC,
-        KC_NO,         KC_Y,         KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
+        KC_EQL,        KC_6,         KC_7,   KC_8,   KC_9,   KC_0,             KC_BSPC,
+        KC_RBRC,       KC_Y,         KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                        KC_H,         KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
         KC_LANG2,      KC_N,         KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                                      KC_MINS,KC_EQL, KC_LBRC,KC_RBRC,          MO(SYMB),
