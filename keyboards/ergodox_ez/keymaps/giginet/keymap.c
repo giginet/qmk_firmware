e1a6e0e8d22866fcba2bdd1a1feb4304987fab48
giginet <giginet.net@gmail.com>
2018-07-24 16:20:01 +0900

    Add cursor keys to layer 0

--- keyboards/ergodox_ez/keymaps/giginet/keymap.c
+++ keyboards/ergodox_ez/keymaps/giginet/keymap.c
@@ -17,18 +17,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | `      |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   '"   |
+ * |LCtl/Tab|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   '"   |
  * |--------+------+------+------+------+------| Kana |           | Alpha|------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |~L1   |  '"  | LCtrl|Option|  Cmd |                                       |  -_  |  =+  |   [  |   ]  | ~L1  |
+ *   |~L1   |  '"  | LCtrl|Option| Cmd  |                                       |  -_  |  =+  |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,---------------.       ,---------------.
- *                                        |Ctrl/Esc| Alt  |       | Alt  |Ctrl/Esc|
+ *                                        | Left | Right  |       | Left | Right  |
  *                                 ,------|------|--------|       |------+--------+------.
- *                                 |      |      |  Kana  |       | Alpha|        |      |
+ *                                 |      |      |   Up   |       |  Up  |        |      |
  *                                 | Space| Cmd  |--------|       |------|  Cmd   |Enter |
- *                                 |      |      | ~MDIA  |       | ~SYMB|        |      |
+ *                                 |      |      |  Down  |       | Down |        |      |
  *                                 `----------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -39,19 +39,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_NO,
         CTL_T(KC_TAB), KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,       CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   KC_LANG1,
-        MO(SYMB),      KC_QUOT,      KC_LCTL,KC_LALT,KC_LGUI,
-                                                     CTL_T(KC_ESC),KC_LALT,
-                                                             KC_LANG1,
-                                             KC_SPC,KC_LGUI, MO(MDIA),
+        MO(MDIA),      KC_QUOT,      KC_LCTL,KC_LALT,KC_LGUI,
+                                                     KC_LEFT,KC_RIGHT,
+                                                             KC_UP,
+                                             KC_SPC,KC_LGUI, KC_DOWN,
         // right hand
         KC_NO,         KC_6,         KC_7,   KC_8,   KC_9,   KC_0,             KC_BSPC,
         KC_NO,         KC_Y,         KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                        KC_H,         KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
         KC_LANG2,      KC_N,         KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                                      KC_MINS,KC_EQL, KC_LBRC,KC_RBRC,          MO(SYMB),
-        KC_RALT,  CTL_T(KC_ESC),
-        KC_LANG2,
-        MO(SYMB), KC_RGUI, KC_ENT
+        KC_LEFT,  KC_RIGHT,
+        KC_UP,
+        KC_DOWN,  KC_RGUI, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
