2a182f19eb0c0537008124521e3103707c33210e
giginet <giginet.net@gmail.com>
2018-08-18 10:26:21 +0900

    Modify modifier keys position

--- keyboards/ergodox_ez/keymaps/giginet/keymap.c
+++ keyboards/ergodox_ez/keymaps/giginet/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         CTL_T(KC_TAB), KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,       CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   KC_LANG1,
         MO(MDIA),      KC_QUOT,      KC_LCTL,KC_LALT,KC_LGUI,
-                                                     KC_LEFT,KC_RIGHT,
+                                                     KC_LCTL,KC_LALT,
                                                              KC_UP,
                                              KC_SPC,KC_LGUI, KC_DOWN,
         // right hand
